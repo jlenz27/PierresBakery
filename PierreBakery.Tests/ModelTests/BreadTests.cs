0022079a4ed0b4d810f2e8a51e1ec970ba348f05
John Lenz <john.lenz27@gmail.com>
2023-02-24 11:16:35 -0800

    good test 2 fail to test the price of one bread

--- PierreBakery.Tests/ModelTests/BreadTests.cs
+++ PierreBakery.Tests/ModelTests/BreadTests.cs
@@ -10,10 +10,19 @@ namespace PierreBakery.Tests
 
         public void PriceConstructor_CreatesInstanceOfBread_Bread()
         {
-            Bread newPrice = new Bread();
+            Bread newPrice = new Bread(5);
             Assert.AreEqual(typeof(Bread), newPrice.GetType());
         }
-   
+        [TestMethod]
+
+        public void breadPrice_ReturnsPriceOfBread_int()
+        {
+            Bread newPrice = new Bread(5);
+            int expectedPrice = 5;
+            int userQuantity = 1;
+            int actualPrice = newPrice.breadPrice(userQuantity);
+            Assert.AreEqual(expectedPrice, actualPrice);
+        }
 
     }
 }
